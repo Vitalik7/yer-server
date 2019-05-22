43ab0b64c6ab2f428a84df98b2659362dee9f989
Vitalik7 <lavrenko.vitalik@yandex.ua>
2019-05-22 22:47:09 +0300

    change host port

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const host = 'localhost';
 
 module.exports = {
-    port: process.env.port || process.env.PORT || '3000',
+    port: process.env.port || process.env.PORT || '8080',
      mongoUrl: process.env.MONGODB_URI || `mongodb://test:test123@ds135844.mlab.com:35844/yer-wedding`  // Vitalik db
 
     };
